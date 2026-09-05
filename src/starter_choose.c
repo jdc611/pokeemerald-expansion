50b4419a41501ee3ee6f0f54df1f08f45c13a37a
jdc611 <jdcoleman611@gmail.com>
2026-09-04 19:35:35 -0500

    Update starter_choose.c

--- src/starter_choose.c
+++ src/starter_choose.c
@@ -126,10 +126,7 @@ static void GenerateRandomStarters(void)
         .arg2 = FILTER_FUNC_ARG_NONE,
     };
 
-    if (gSaveBlock3Ptr->worldSeed == 12345678)
-    SeedRng(12345678);
-else
-    SeedRng(87654321);
+    SeedRng(gSaveBlock3Ptr->worldSeed);
 
     for (u32 i = 0; i < STARTER_MON_COUNT; i++)
     {
