8979b39a634373d36a3c08f7b9c059b3f028945d
jdc611 <jdcoleman611@gmail.com>
2026-09-04 22:14:45 -0500

    Update start_menu.c
    Make dexnav available from start

--- src/start_menu.c
+++ src/start_menu.c
@@ -327,8 +327,8 @@ static void BuildNormalStartMenu(void)
     if (FlagGet(FLAG_SYS_POKEDEX_GET) == TRUE)
         AddStartMenuAction(MENU_ACTION_POKEDEX);
 
-    if (DN_FLAG_DEXNAV_GET != 0 && FlagGet(DN_FLAG_DEXNAV_GET))
-        AddStartMenuAction(MENU_ACTION_DEXNAV);
+    if (DEXNAV_ENABLED)
+    AddStartMenuAction(MENU_ACTION_DEXNAV);
 
     if (FlagGet(FLAG_SYS_POKEMON_GET) == TRUE)
         AddStartMenuAction(MENU_ACTION_POKEMON);
