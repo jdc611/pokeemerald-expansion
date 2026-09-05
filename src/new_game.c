a1e340b61814067c61286273ef7e631d875d86f1
jdc611 <jdcoleman611@gmail.com>
2026-09-05 13:24:32 -0500

    Update new_game.c

--- src/new_game.c
+++ src/new_game.c
@@ -235,8 +235,7 @@ void NewGameInitData(void)
     ResetItemFlags();
     ResetDexNav();
     gSaveBlock3Ptr->worldSeed = 12345678;
-    
-    gSaveBlock3Ptr->randomizerEnabled = TRUE;
+       
 gSaveBlock3Ptr->starterMode = RUN_STARTER_RANDOM;
 gSaveBlock3Ptr->rivalMode = RUN_RIVAL_NORMAL;
 gSaveBlock3Ptr->filterMode = RUN_FILTER_NONE;
