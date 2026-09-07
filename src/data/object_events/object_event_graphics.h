409a0556cb03e7bc7ad6faeb6adc212fe3a7cc8a
jdc611 <jdcoleman611@gmail.com>
2026-09-07 00:44:46 -0500

    Update object_event_graphics.h

--- src/data/object_events/object_event_graphics.h
+++ src/data/object_events/object_event_graphics.h
@@ -62,6 +62,7 @@ const u16 gObjectEventPal_Npc1[] = INCGFX_U16("graphics/object_events/palettes/n
 const u16 gObjectEventPal_Npc2[] = INCGFX_U16("graphics/object_events/palettes/npc_2.pal", ".gbapal");
 const u16 gObjectEventPal_Npc3[] = INCGFX_U16("graphics/object_events/palettes/npc_3.pal", ".gbapal");
 const u16 gObjectEventPal_Npc4[] = INCGFX_U16("graphics/object_events/palettes/npc_4.pal", ".gbapal");
+const u16 gObjectEventPal_Jack[] = INCGFX_U16("graphics/object_events/palettes/jack.pal", ".gbapal");
 const u16 gObjectEventPal_Npc1Reflection[] = INCGFX_U16("graphics/object_events/palettes/npc_1_reflection.pal", ".gbapal");
 const u16 gObjectEventPal_Npc2Reflection[] = INCGFX_U16("graphics/object_events/palettes/npc_2_reflection.pal", ".gbapal");
 const u16 gObjectEventPal_Npc3Reflection[] = INCGFX_U16("graphics/object_events/palettes/npc_3_reflection.pal", ".gbapal");
