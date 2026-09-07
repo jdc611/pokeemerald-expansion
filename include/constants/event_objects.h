76cde175ca715ed06b827e312c2f876c254cc121
jdc611 <jdcoleman611@gmail.com>
2026-09-07 00:38:45 -0500

    Update event_objects.h

--- include/constants/event_objects.h
+++ include/constants/event_objects.h
@@ -604,8 +604,8 @@ enum
 #define OBJ_EVENT_PAL_TAG_BALL_BEAST              0x1169
 // Gen VIII
 #define OBJ_EVENT_PAL_TAG_BALL_STRANGE            0x116A
-#define OBJ_EVENT_PAL_TAG_JACK                    0x116B
 #endif //OW_FOLLOWERS_POKEBALLS
+#define OBJ_EVENT_PAL_TAG_JACK                    0x116B
 // Used as a placeholder follower graphic
 #define OBJ_EVENT_PAL_TAG_SUBSTITUTE              0x7611
 #define OBJ_EVENT_PAL_TAG_LIGHT                   0x8001
