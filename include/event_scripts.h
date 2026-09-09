63fe9c9f14d9ab336e6d85386fe87c46cb93665e
jdc611 <jdcoleman611@gmail.com>
2026-09-08 20:27:00 -0500

    Update event_scripts.h

--- include/event_scripts.h
+++ include/event_scripts.h
@@ -597,6 +597,7 @@ extern const u8 EventScript_UseDive[];
 extern const u8 EventScript_UseDiveUnderwater[];
 extern const u8 EventScript_UseRockClimb[];
 extern const u8 EventScript_UsePokeVial[];
+extern const u8 EventScript_ChangeNature[];
 extern const u8 EventScript_FallDownHole[];
 extern const u8 EventScript_FieldPoison[];
 extern const u8 EventScript_EggHatch[];
