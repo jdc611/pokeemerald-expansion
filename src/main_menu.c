8e9ee3b18ed89caff908188ed558a110c589e787
jdc611 <jdcoleman611@gmail.com>
2026-09-19 06:49:55 -0500

    Use Eevee sprite throughout Birch introduction

--- src/main_menu.c
+++ src/main_menu.c
@@ -2780,7 +2780,7 @@ static void SpriteCB_MovePlayerDownWhileShrinking(struct Sprite *sprite)
 
 static u8 NewGameBirchSpeech_CreateLotadSprite(u8 x, u8 y)
 {
-    return CreateMonPicSprite_Affine(SPECIES_SCYTHER, FALSE, 0, MON_PIC_AFFINE_FRONT, x, y, 14, TAG_NONE);
+    return CreateMonPicSprite_Affine(SPECIES_EEVEE, FALSE, 0, MON_PIC_AFFINE_FRONT, x, y, 14, TAG_NONE);
 }
 
 static void AddBirchSpeechObjects(u8 taskId)
