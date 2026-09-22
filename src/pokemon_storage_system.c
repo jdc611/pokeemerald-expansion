a45402dbae5b10b789054f5550d743f6cea08963
jdc611 <jdcoleman611@gmail.com>
2026-09-21 20:13:18 -0500

    Allow party repairs while inside Pokemon Centers

--- src/pokemon_storage_system.c
+++ src/pokemon_storage_system.c
@@ -2804,8 +2804,8 @@ static void Task_WithdrawMon(u8 taskId)
             PrintMessage(MSG_PARTY_FULL);
             sStorage->state = 1;
         }
-        else if (!DoesSpeciesMatchActiveRunFilter(species)
-              || (!IsPlayerInPokemonCenter() && !DoesBoxMonMatchActiveRunFilter(GetBoxedMonPtr(StorageGetCurrentBox(), sCursorPosition))))
+        else if (!IsPlayerInPokemonCenter()
+              && !DoesBoxMonMatchActiveRunFilter(GetBoxedMonPtr(StorageGetCurrentBox(), sCursorPosition)))
         {
             PrintMessage(MSG_RUN_FILTER_BLOCKED);
             sStorage->state = 1;
