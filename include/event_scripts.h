b5cdad049549ce452cd26355fa0a8cbd5986c079
jdc611 <jdcoleman611@gmail.com>
2026-09-22 19:55:53 -0500

    Remove obsolete filtered Ability Changer script declaration

--- include/event_scripts.h
+++ include/event_scripts.h
@@ -602,7 +602,6 @@ extern const u8 EventScript_UsePokeVial[];
 extern const u8 EventScript_ChangeNature[];
 extern const u8 EventScript_ChangeGender[];
 extern const u8 EventScript_ChangeAbility[];
-extern const u8 EventScript_ChangeAbilityForRunFilter[];
 extern const u8 EventScript_FallDownHole[];
 extern const u8 EventScript_FieldPoison[];
 extern const u8 EventScript_EggHatch[];
