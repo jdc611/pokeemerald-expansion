e2eef4f5ba756ebbffb559419b0540579b41e5a0
jdc611 <jdcoleman611@gmail.com>
2026-09-25 15:52:46 -0500

    Map Surf and Waterfall to new field artwork

--- src/item_icon.c
+++ src/item_icon.c
@@ -168,7 +168,8 @@ const void *GetItemIconPic(enum Item itemId)
         case ITEM_HM_ROCK_SMASH: return gItemIcon_ChaosRockSmash;
         case ITEM_HM_STRENGTH:   return gItemIcon_ChaosStrength;
         case ITEM_HM_FLASH:      return gItemIcon_ChaosFlash;
-        case ITEM_HM_SURF:        return gItemIcon_ChaosSurf;
+        case ITEM_HM_SURF:        return gItemIcon_ChaosSplash;
+        case ITEM_HM_WATERFALL:   return gItemIcon_ChaosWaterfall;
         default: break;
         }
         if (GetItemTMHMIndex(itemId) > NUM_TECHNICAL_MACHINES)
@@ -193,7 +194,8 @@ const u16 *GetItemIconPalette(enum Item itemId)
         case ITEM_HM_ROCK_SMASH: return gItemIconPalette_ChaosRockSmash;
         case ITEM_HM_STRENGTH:   return gItemIconPalette_ChaosStrength;
         case ITEM_HM_FLASH:      return gItemIconPalette_ChaosFlash;
-        case ITEM_HM_SURF:        return gItemIconPalette_ChaosSurf;
+        case ITEM_HM_SURF:        return gItemIconPalette_ChaosSplash;
+        case ITEM_HM_WATERFALL:   return gItemIconPalette_ChaosWaterfall;
         default: break;
         }
         return gTypesInfo[GetMoveType(GetItemTMHMMoveId(itemId))].paletteTMHM;
